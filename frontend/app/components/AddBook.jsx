0c85a5e603ad246895fd3066be124af5b1367b1f
adityach0uhan <m4with6x@gmail.com>
2023-12-28 00:11:49 +0530

    Added some style to form

--- frontend/app/components/AddBook.jsx
+++ frontend/app/components/AddBook.jsx
@@ -6,23 +6,28 @@ const AddBook = () => {
     const [author, setauthor] = useState("")
     const [publishYear, setpublishYear] = useState("")
 
-    const bookTitle = (e) => {
-        console.log(bookTitle)
+    const handelSubmit = () => {
+        console.log(title)
     }
 
     return (
 
         <>
             <div className='flex p-10 flex-col align-center justify-center w-full '>
-                <span>Add More Books</span>
-                <form action="" method="POST" className='flex-col flex'>
-                    <label htmlFor="title" className='w-60 h-10 rounded'>Title</label>
-                    <input type="text" className='w-60 h-10 rounded' onChange={(e)=>{bookTitle}} id='title' />
-                    <label htmlFor="author" className='w-60 h-10 rounded'>Author</label>
-                    <input type="text" className='w-60 h-10 rounded' id='author' onChange={() => { bookAuthor }} />
-                    <label htmlFor="publish " className='w-60 h-10 rounded'>Publish Year</label>
-                    <input type="text" id='publish' className='w-60 h-10 rounded' onChange={() => { bookpublishYear }} />
-                    <button type='submit' className='w-20 h-10 rounded border border-black border-1'>Add Book</button>
+                <span className='text-center text-2xl'>Add More Books</span>
+                <form className='flex-col flex m-auto'>
+                    <div className='w-100 border-black rounded border-1 border p-5'>
+                        <label htmlFor="title" className='w-60 h-10 mr-9 rounded text-center'>Title</label>
+                        <input type="text" className='w-60 h-10 rounded' onChange={(e) => settitle(e.target.value)} id='title' />
+                    </div>
+                    <div className='w-100 border-black rounded border-1 border p-5'>
+                        <label htmlFor="author" className='w-60 h-10  mr-9  rounded text-center'>Author</label>
+                        <input type="text" className='w-60 h-10 rounded' id='author' onChange={(e) => { setauthor(e.target.value) }} />
+                    </div>
+                    <div className='w-100 border-black rounded border-1 border p-5'>
+                        <label htmlFor="publish " className='w-60  mr-9  h-10 rounded text-center'>Publish Year</label>
+                        <input type="text" id='publish' className='w-60 h-10 rounded' onChange={(e) => { setpublishYear(e.target.value) }} /></div>
+                    <button onClick={() => handelSubmit} className='w-20 h-10 rounded border border-black border-1'>Add Book</button>
                 </form>
 
             </div>

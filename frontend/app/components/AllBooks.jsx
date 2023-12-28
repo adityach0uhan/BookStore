5c24041a49475bee9326bdc86f0911e2bc5cc706
adityach0uhan <m4with6x@gmail.com>
2023-12-28 23:37:24 +0530

    Some style changes in table

--- frontend/app/components/AllBooks.jsx
+++ frontend/app/components/AllBooks.jsx
@@ -13,16 +13,16 @@ const AllBooks = ({ booklist }) => {
 
     return (
         <>
-            <div className='  m-auto text-center'>
+            <div className='  m-auto text-center '>
                 <h2 className='text-2xl m-10'>Book Store</h2>
-                <table className='table-fixed border-collapse   m-auto border-spacing-2 border border-slate-500'>
+                <table className='table-fixed m-auto  '>
                     <thead className=''>
-                        <tr className='flex box-border mb-6 items-center justify-self-start gap-5 border-black'>
-                            <th className='min-w-40 capitalize'>No.</th>
-                            <th className='min-w-40 capitalize'>Title</th>
-                            <th className='min-w-40 capitalize'>author</th>
-                            <th className='min-w-40 capitalize'>Publish Year</th>
-                            <th className='min-w-40 capitalize'>Task</th>
+                        <tr className='flex box-border mb-6 items-center justify-self-start gap-5 '>
+                            <th className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black'>No.</th>
+                            <th className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black'>Title</th>
+                            <th className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black'>author</th>
+                            <th className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black'>Publish Year</th>
+                            <th className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black'>Task</th>
                         </tr>
                     </thead>
                     <tbody >
@@ -30,14 +30,14 @@ const AllBooks = ({ booklist }) => {
                             booklist.map((item, index) => {
                                 return (
 
-                                    <tr key={index} className='box-border flex items-center justify-start gap-5 border-black flex gap-5 border border-slate-700 '>
-                                        <td className='min-w-40 capitalize'>{index + 1}</td>
-                                        <td className='min-w-40 capitalize'>{item.title}</td>
-                                        <td className='min-w-40 capitalize'>{item.author}</td>
-                                        <td className='min-w-40 capitalize'>{item.publishYear}</td>
-                                        <td className='min-w-20 capitalize '>
-                                            <button onClick={() => handelDelete(item._id)} className='w-20 border-black border m-2 bg-red-400 rounded'><FontAwesomeIcon icon={faTrash} /></button>
-                                            <button onClick={() => handelUpdate(item._id)} className='w-20 border-black border m-2 bg-green-400 rounded'><FontAwesomeIcon icon={faPencil} /></button>
+                                    <tr key={index} className='box-border flex items-center justify-start gap-5 flex '>
+                                        <td className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black capitalize'>{index + 1}</td>
+                                        <td className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black capitalize'>{item.title}</td>
+                                        <td className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black capitalize'>{item.author}</td>
+                                        <td className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black capitalize'>{item.publishYear}</td>
+                                        <td className='min-w-48 capitalize p-2 m-2 rounded border border-1 border-black capitalize '>
+                                            <button onClick={() => handelDelete(item._id)} className='w-16 border-black border mr-3  bg-red-400 rounded'><FontAwesomeIcon icon={faTrash} /></button>
+                                            <button onClick={() => handelUpdate(item._id)} className='w-16 border-black border bg-green-400 rounded'><FontAwesomeIcon icon={faPencil} /></button>
                                         </td>
                                     </tr>
                                 )
--- frontend/app/components/Loader.jsx
+++ frontend/app/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-      <div className='flex w-screen h-screen   bg-white'>
+      <div className='flex w-screen h-screen'>
           <div className='h-40 w-40 m-auto' >
               <img className='h-30 w-30' src="loader.gif" alt="" />
           </div>

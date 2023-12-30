dc7b581107b7b3c8a73cc0a4d3dcae878dbd9389
adityach0uhan <m4with6x@gmail.com>
2023-12-30 21:30:04 +0530

    design changes in create book ui

--- frontend/app/addBook/page.jsx
+++ frontend/app/addBook/page.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const AddBook = () => {
 
   const [title, settitle] = useState("")
   const [author, setauthor] = useState("")
   const [publishYear, setpublishYear] = useState("")
-  const router=useRouter()
+  const router = useRouter()
 
   const handelSubmit = () => {
 
@@ -22,7 +24,7 @@ const AddBook = () => {
       publishYear: publishYear
     }
     axios.post("http://localhost:5000/books/", data).then(() => {
-     router.back()
+      router.back()
     }).catch((err) => {
       alert("Some error occurred ! , book not creare")
       console.log(err)
@@ -34,21 +36,31 @@ const AddBook = () => {
 
   return (
     <>
-      <div className='flex p-10 flex-col align-center justify-center w-full '>
-        <span className='text-center text-2xl'>Add More Books</span>
-        <div className='w-100 border-black rounded border-1 border p-5'>
-          <label htmlFor="title" className='w-60 h-10 mr-9 rounded text-center'>Title</label>
-          <input type="text" className='w-60 h-7 p-3 rounded' value={title} onChange={(e) => { settitle(e.target.value) }} id='title' />
+      <div className='flex p-5 flex-col items-center justify-center w-full '>
+        <span className='text-center text-2xl p-8'>Add More Books</span>
+        <div className='w-1/3 border-black rounded border-1 flex items-center justify-center gap-5  border p-2 mb-2'>
+          <div className=' w-1/4 h-full  rounded text-center'><span className='text-center'>Title</span></div>
+          <div className=' w-3/4 h-full rounded '>
+            <input type="text" className='w-full h-7  border text-center border-1 border-black text-l rounded' value={title} onChange={(e) => { settitle(e.target.value) }} id='title' />
+          </div>
+
         </div>
-        <div className='w-100 border-black rounded border-1 border p-5'>
-          <label htmlFor="author" className='w-60 h-10  mr-9  rounded text-center'>Author</label>
-          <input type="text" className='w-60 h-7 p-3 rounded' id='author' value={author} onChange={(e) => { setauthor(e.target.value) }} />
+        <div className='w-1/3 border-black rounded border-1 flex items-center justify-center gap-5  border p-2 mb-2'>
+          <div className=' w-1/4 h-full  rounded text-center'><span className='text-center'>Author</span></div>
+          <div className=' w-3/4 h-full rounded '>
+
+            <input type="text" className='w-full h-7  border text-center border-1 border-black text-l rounded' id='author' value={author} onChange={(e) => { setauthor(e.target.value) }} />
+            </div>
         </div>
-        <div className='w-100 border-black rounded border-1 border p-5'>
-          <label htmlFor="publish " className='w-60  mr-9  h-10 rounded text-center'>Publish Year</label>
-          <input type="text" id='publish' className='w-60 h-7 p-3 rounded' value={publishYear} onChange={(e) => { setpublishYear(e.target.value) }} /></div>
-        <div className='mt-4 w-full flex justify-center'>
-          <button onClick={() => handelSubmit()} className='w-30 m-auto h-5 p-4 text-sm rounded border border-black border-1'><span className='m-auto h-full w-full'>Add Book</span></button>
+        <div className='w-1/3 border-black rounded border-1 flex items-center justify-center gap-5  border p-2 mb-2'>
+          <div className=' w-1/4 h-full  rounded text-center'><span className='text-center'>Publish year</span></div>
+          <div className=' w-3/4 h-full rounded '>
+          <input type="text" id='publish' className='w-full h-7  border text-center border-1 border-black text-l rounded' value={publishYear} onChange={(e) => { setpublishYear(e.target.value) }} /></div></div>
+        <div className='w-1/3 border-black rounded border-1 flex items-center justify-center gap-5  border p-2 mb-2'>
+          <div className=' w-3/4 h-full rounded '>
+
+            <button onClick={() => handelSubmit()} className='w-full m-auto h-9  text-sm  text-center rounded border border-black border-1'><span className='m-auto h-full w-full'>Add Book <FontAwesomeIcon className='ml-2' icon={faPlus} /></span></button>
+            </div>
         </div>
       </div>
     </>
--- frontend/app/deleteBook/[id]/page.jsx
+++ frontend/app/deleteBook/[id]/page.jsx
@@ -20,11 +20,11 @@ const DeletePage = () => {
 
   }
   return (
-    <div className="w-full h-screen m-auto bg-red-400">
-      <div className=" ">
+    <div className="w-full h-screen flex items-center justify-center ">
+      <div className="bg-red-400 m-auto rounded p-5 w-80 h-40 text-center">
         <p> Are you sure you want to delete this book</p>
-        <button className='w-16 border-black border bg-green-400 hover:bg-green-500 rounded' > no</button>
-        <button className='w-16 border-black border bg-green-400 hover:bg-green-500 rounded' onClick={() => deleteBook()} > yes</button>
+        <button className='w-16 m-4 border-black border bg-green-400 hover:bg-green-500 rounded' onClick={()=> router.back()} > no</button>
+        <button className='w-16 m-4 border-black border bg-green-400 hover:bg-green-500 rounded' onClick={() => deleteBook()} > yes</button>
       </div>
 
     </div>

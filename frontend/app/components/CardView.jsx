5025b8c4e7ba2b3379787ab5dac63f2ed4fe2c16
adityach0uhan <m4with6x@gmail.com>
2023-12-31 14:26:55 +0530

    fixed grid coloumn glitch

--- frontend/app/components/CardView.jsx
+++ frontend/app/components/CardView.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 export const CardView = ({ booklist }) => {
     return (
         <>
-            {
-                booklist.map((data, index) => {
+            <div className='w-screen h-full gap-3 p-2 grid grid-cols-4 '>
+                {
+                    booklist.map((data, index) => {
 
-                    return  <div className='w-full h-full grid grid-cols-4 gap-4 bg-blue-500'>
-                        <div className='w-62 h-40 bg-red-700 rounded'>
-                            <span>{data._id}</span>
+                        return <div className='w-62 h-40 bg-red-700 rounded'>
+                            <div>{data._id}</div>
+                            <div>Title : {data.title}</div>
+                            <div>Author : {data.author}</div>
+                            <div>Publish Year : {data.publishYear}</div>
                         </div>
-                    </div>
 
-                })
-                
-            }
+                    })
+
+                }
+            </div>
 
 
 
